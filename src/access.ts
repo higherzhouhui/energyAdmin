9338f99ebaf3427030f7ef178dd2c17d402af921
higherzhouhui <782492184@qq.com>
2023-06-12 21:57:52 +0800

    fix bug

--- src/access.ts
+++ src/access.ts
@@ -4,7 +4,7 @@
 export default function access(initialState: { currentUser?: API.CurrentUser | undefined }) {
   const { currentUser } = initialState || {};
   return {
-    adminRouteFilter: () => currentUser?.type === 1 || currentUser?.comments !== 'service',
-    serviceRouteFilter: () => currentUser?.comments === 'service' || currentUser?.comments === 'developer',
+    adminRouteFilter: () => currentUser?.type == 1 || currentUser?.comments !== 'service',
+    serviceRouteFilter: () => currentUser?.comments === 'service' || currentUser?.comments === 'developer' || currentUser?.type == 1,
   };
 }
